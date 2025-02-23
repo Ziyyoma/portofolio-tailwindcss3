6c6e0e9bf90a4693fe05e840b55b2ade3c99fd37
Ziyyo Maulana <ziyoomplong82@gmail.com>
2025-02-23 14:50:42 +0700

    update dark mode + perbaikan fitur

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['index.html'],
+  darkMode: 'class', 
   theme: {
     container:{
       center: true,
@@ -10,7 +11,8 @@ module.exports = {
       colors: {
         primary: '#0c4a6e',
         dark: '#0f172a',
-        secondary: '#64748b'
+        secondary: '#64748b',
+        green: '#14b8a6'
       },
       screens: {
         'xl': '1920px',
